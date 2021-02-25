509041b3173de98b7a50a2f838a69b14c6075a8b
tussath <ussath@youbility.de>
2021-02-25 13:56:03 +0100

    change archive script

--- .createZip.sh
+++ .createZip.sh
@@ -4,8 +4,6 @@ pluginversion=$(sed 's/.*"version": "\(.*\)".*/\1/;t;d' ./composer.json)
 
 filename=semknox-magento2_$pluginversion.zip
 
-sed -r 's/(setup_version=")[^"]+"/\1$pluginversion"/' extensions/semknox/semknox-magento2/etc/module.xml
-
 cd ../..
 
 if [ -e $filename ]; then
@@ -13,5 +11,7 @@ if [ -e $filename ]; then
 fi
 
 cd ..
+sed -i 's/setup_version="[^"]*"/setup_version="'$pluginversion'"/' extensions/semknox/semknox-magento2/etc/module.xml
+
 zip -rq extensions/$filename extensions -x 'extensions/semknox/semknox-core/examples/*' -x 'extensions/semknox/semknox-core/tests/*' -x '*/codeception.yml' -x '*/.*' -x '*.zip'
 echo "$filename Archive created."
