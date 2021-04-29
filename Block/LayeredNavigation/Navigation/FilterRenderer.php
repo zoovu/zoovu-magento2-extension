8484898c947d4c060a2e302abfe349dddf49240d
tussath <ussath@youbility.de>
2021-04-29 13:14:09 +0200

    wip

--- Block/LayeredNavigation/Navigation/FilterRenderer.php
+++ Block/LayeredNavigation/Navigation/FilterRenderer.php
@@ -28,10 +28,48 @@ class FilterRenderer extends \Magento\LayeredNavigation\Block\Navigation\FilterR
     )
     {
         $this->_sxHelper = $sxHelper;
+        $this->_isSxSearch = $sxHelper->isSearch() && $sxHelper->isSxSearchFrontendActive();
+        
         $this->_filterItem = $filterItemFactory;
 
         $this->_sxFilters = $this->_sxHelper->getSxResponseStore('filterList', []);
         parent::__construct($context, $data);
+    }
+
+
+    /**
+     * @param FilterInterface $filter
+     * @return string
+     */
+    public function render($filter)
+    {
+        if (!$this->_isSxSearch) return parent::render($filter);
+
+        // if range.. do nouislider
+        $rangeFilter = $this->_sxHelper->getSxResponseStore('rangeFilter', []);
+
+        if (count($rangeFilter) && is_string($filter->getName()) && isset($rangeFilter[$filter->getName()])) {
+            $sxFilter = $rangeFilter[$filter->getName()];
+
+            return "<div class='slider-wrapper'>
+                        <div class='slider sxRangeFilter' id='sx_" . $filter->getName() . "' 
+                            data-start='" . $sxFilter->getActiveMin() . "'
+                            data-end='" . $sxFilter->getActiveMax() . "'
+                            data-range-min='" . $sxFilter->getMin() . "'
+                            data-range-max='" . $sxFilter->getMax() . "'
+                            data-url='" . $filter->getRemoveUrl() . "'
+                        ></div>
+                        <div class='slider-helper'>
+                            <input class='start' value='' min='' type='number' name='num1'>
+                            <span>-</span>
+                            <input class='end' value='' max='' type='number' name='num2'>
+                            <span class='unit'>" . $sxFilter->getUnit() . "</span>
+                            <button class='' type='button'><i class='fa fa-angle-right'></i></button>
+                        </div>
+                    </div>";
+        }
+
+        return parent::render($filter);;
     }
 
     /**
